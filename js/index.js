ce44197620552110c89e8c150afb4c3e539b89fd
NymaXx <53951684+NymaXx@users.noreply.github.com>
2021-04-22 16:55:34 -0500

    archivo js botones y anadido de candidatos basico

--- js/index.js
+++ js/index.js
@@ -1,33 +1,48 @@
-const ETname = document.getElementById('ETname');
-const ETcedula = document.getElementById('ETcedula');
+//para registrar
+const IDreg = document.getElementById('IDreg');
+const nameReg = document.getElementById('nameReg');
 const ETbotonRegistrar = document.getElementById('ETbotonRegistrar');
 
+//para votar
+const idVote = document.getElementById('idVote');
+const voteButton = document.getElementById('voteButton');
 
+//para ver los nombres y los votos
+const verNombresButton = document.getElementById('verNombresButton');
+const verVotosButton = document.getElementById('verVotosButton');
+
+//objeto database
 const database = firebase.database();
 
 
-const obtainData = ()=> {
-  
-    let nombre = ETname.value;
-    let cedula = ETcedula.value;
+registrarCandidato = () => {
+    let id = IDreg.value;
+    let nombre = nameReg.value;
 
-    console.log(nombre);
-    console.log(cedula);
-    alert(nombre + " " + cedula);
-
-
-    let obj = {nombre:nombre, cedula:cedula};
-
-    database.ref('Users' + nombre).push(obj);
-
-
+    let obj = {ID: id, Nombre: nombre};
+    database.ref('Candidatos/' + id).set(obj);
+    //database.ref('Candidatos/A1').set(obj);
 }
 
-ETbotonRegistrar.addEventListener('click', obtainData);
-
-database.ref('Users').on('value', (data)=>{
-    data.forEach(
-        user => {
-        console.log(user.val());
+verCandidatos= () => {
+    database.ref('Candidatos').on('value', (data) => {
+        data.forEach(
+            (id)=>{
+                console.log(id.val());
+            }
+        );
     });
-});
+}
+
+verVotos = () => {
+    database.ref('Candidatos').on('value', (votes) => {
+        console.log(votes.val());
+        alert(votes.val());
+    });
+}
+
+//listeners de los botones
+ETbotonRegistrar.addEventListener('click', registrarCandidato);
+verNombresButton.addEventListener('click',verCandidatos);
+verVotosButton.addEventListener('click',verVotos);
+
