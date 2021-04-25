875583c47b67f0a6a649f65d7ee90b293c375159
NymaXx <53951684+NymaXx@users.noreply.github.com>
2021-04-25 16:54:51 -0500

    finalizado (no funciona bien lo de los votos) registro completo y exitoso'

--- js/index.js
+++ js/index.js
@@ -15,82 +15,119 @@ const verVotosButton = document.getElementById('verVotosButton');
 const database = firebase.database();
 
 
-registrarCandidato = () => {
-    let id = IDreg.value;
-    let nombre = nameReg.value;
+    registrarCandidato = () => {
+        let id = IDreg.value;
+        let nombre = nameReg.value;
 
-    let obj = {ID: id, Nombre: nombre};
-    let objVotes = {ID: id, Cantidad: 0};
-    database.ref('Candidatos/' + id).set(obj);
-    //database.ref('Votos/' + id).set(objVotes);
-
-}
-
-verCandidatos= () => {
-    database.ref('Candidatos').on('value', (data) => {
-        data.forEach(
-            (id)=>{
-                let list = id.val().Nombre;
-                alert(list);
-            }
-        );
-    });
-}
-
-
-    /*const alfa = ()=>{
-    
-    database.ref('votos').on('value', (data)=>{
         
-        var output = "";    
-        data.forEach(child => {
-            console.log(child.key);
-            console.log(child.val());
-            //la siguiente es para contar cuantos atributos hay, asi contamos los votos con el lenght
-            let numVotos = Object.entries(child.val()).length;
-            console.log(numVotos);
-            //child.key es el identificador del candidato
-            output += child.key+": "+numVotos;
+
+        if(validarCandidato){
+            let obj = {ID: id, Nombre: nombre};
+            database.ref('Candidatos/' + id).set(obj);
+            //database.ref('Votos/' + id).set(objVotes);
+        }else{
+            alert('El id del candidato que intenta registrar ya existe');
+        }
+
+    }
+
+    validarCandidato = () => { //para evitar el doble registro de candidatos
+        let nuevoCandidato = true;
+        let idR = IDreg.value
+        database.ref('Candidatos').on('value', (data) => {
+            data.forEach(
+                (nom) => {
+
+                    let canID = nom.val();
+                    let id = canID.id;
+                    
+                    if(id == idR){
+
+                        nuevoCandidato = false;
+                    }
+                }
+            );
         });
-        console.log(output);
-    });
-
-}
-alfa();*/
+        return nuevoCandidato;
+    }
 
 
-//modo dos del profe
-const beta = ()=>{
+    verCandidatos= () => {
+        let nombresLista= [];
+        database.ref('Candidatos').on('value', (data) => {
+            data.forEach(
+                (id)=>{
+                    let list = id.val();
+                    nombresLista.push(list.Nombre+ " " + list.ID + " ");
+                    
+                }
+            );
+            alert(nombresLista);
+        });
+    }
 
-    var output = "";
-    database.ref('cadidatos').once('value', (data)=>{
-        data.forEach(child => {
-            console.log(child.key);
-            console.log(child.val());
-            database.ref('votos/'+child.key).once('value', (votos)=>{
-                votos.forEach(voto=>{
-                    console.log(child.key+"=>"+voto.val());
-                    output += child.key+"=>"+voto.val();
-                });
-                
-            });
+    votar= () => {
+        let idAvotar = idVote.value;
+        let votoRegis ={idVoto:Math.random(), id:idvAvotar}
+
+        let key;
+        let valide = false;
+
+        database.ref('Candidatos').on('value', (data) => {
+            data.forEach(
+                 (c) => {
+                    let val = c.val();
+                    let id = val.id;
+                    if(id == idAvotar){
+                        valide= true;
+                        key = c.key;
+                    }
+                }
+            );
+
+        });
+
+        if(valide===true){
+        database.ref('Votos/' + id).set(votoRegis);
+
+        }else{
+            alert('no existe un candidato con este ID');
+        }
+    }
+
+    mostrarVotos = () => {
+        let votosLista= [];
+        let totalVotos;
+
+        database.ref('Votos').on('value', (data) =>{
+            totalVotos=data.numChildren();
+            //console.log(totalVotos);
+        });
+
+        database.ref('Votos').on('value', (data) => {
+            data.forEach(
+                (c) => {
+                    let valor = c.val();
+                    let key = c.key;
+                    let id = valor.id;
+                    votosLista.push(id +" "+ datav.numChildren()/totalVotos*100+ "%");
+                }
+            )
+        });
+        alert(votosLista);
+    }
+
+ 
 
     
-        });
-        console.log(output);
-    });
-}
-
-beta();
-
-
 
     
 
 //listeners de los botones
 ETbotonRegistrar.addEventListener('click', registrarCandidato);
 verNombresButton.addEventListener('click',verCandidatos);
-//verVotosButton.addEventListener('click', alfa);
+verVotosButton.addEventListener('click', mostrarVotos);
+voteButton.addEventListener('click', votar);
 
 //CODIGO DEL PROFE
 
